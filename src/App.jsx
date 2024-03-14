e63b99d23a16f10b6e854fb6a329906c3409263e
Logan Furedi <furedil@myumanitoba.ca>
2024-03-14 17:50:23 -0500

    Feat: Add Hover Effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
-import { Canvas } from "@react-three/fiber"
-import './App.css'
-import { AmbientLight, BoxGeometry } from "three"
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, Wireframe } from "@react-three/drei"
+import { MeshBasicMaterial } from 'three';
+import './App.css'
+
+const Box = ({ onSelect }) => {
+	const mesh = useRef();
+	const [hovered, setHover] = useState(false);
+	const [selected, setSelected] = useState(false);
+  
+	useFrame(() => (mesh.current.rotation.y += 0.01));
+  
+	return (
+	  <mesh
+		ref={mesh}
+		scale={selected ? [1.2, 1.2, 1.2] : [1, 1, 1]}
+		onClick={(event) => onSelect(event, mesh)}
+		onPointerOver={() => setHover(true)}
+		onPointerOut={() => setHover(false)}>
+		<boxGeometry args={[1, 2, 1]} />
+		<meshBasicMaterial
+		  color={selected ? 'red' : hovered ? 'cyan' : 'blue'}
+		  wireframe={true}
+		/>
+	  </mesh>
+	);
+  };
+  
 
 const App = () => {
   return (
     <Canvas camera = {{position: [0,0,4]}}>
-    	<mesh>
-    		<boxGeometry args={[1, 2, 1]} />
-        	<meshBasicMaterial wireframe color="blue" />
-      	</mesh>
+    	<Box />
 	  	<OrbitControls />
     </Canvas>
   )
