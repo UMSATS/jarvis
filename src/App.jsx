a75ce1f6650a5677f082cd081b85db2b2a3fcc0f
Logan Furedi <furedil@myumanitoba.ca>
2024-03-14 17:21:23 -0500

    Feat: Change Cube Appearance

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import { Canvas } from "@react-three/fiber"
 import './App.css'
-import { BoxGeometry } from "three"
-import { OrbitControls } from "@react-three/drei"
+import { AmbientLight, BoxGeometry } from "three"
+import { OrbitControls, Wireframe } from "@react-three/drei"
 
 const App = () => {
   return (
     <Canvas camera = {{position: [0,0,4]}}>
-      <directionalLight position = {[0,1,1]} />
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial wireframe />
-      </mesh>
-	  <OrbitControls />
+    	<mesh>
+    		<boxGeometry args={[1, 2, 1]} />
+        	<meshBasicMaterial wireframe color="blue" />
+      	</mesh>
+	  	<OrbitControls />
     </Canvas>
   )
 }
